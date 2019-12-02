038f54fd58483c415f925faeee1e3159bf6fd84a
kerryng2000 <33183687+kerryng2000@users.noreply.github.com>
2019-12-01 21:05:56 -0800

    Add files via upload

---
+++ GameOver.java
@@ -0,0 +1,68 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+
+
+/**
+ * 
+ * @author
+ */
+public class GameOver extends Sprite implements Commons {
+
+	private final String gameOver = "/img/gameIsOver.png";
+	private int width;
+	private JButton restartButton;
+
+	/*
+	 * Constructor
+	 */
+	public GameOver() {
+		ImageIcon ii = new ImageIcon(this.getClass().getResource(gameOver));
+
+		setWidth(ii.getImage().getWidth(null));
+
+		setImage(ii.getImage());
+		setX(0);
+		setY(0);
+		
+		add(restartButton);
+	}
+	
+	//add restart button
+	
+			public void addRestartButton() {
+				 
+			    restartButton = new JButton("New Game?");
+			    restartButton.addActionListener(new ButtonListener());
+			    restartButton.setBounds(800, 800, 200, 100);
+			    add(restartButton);
+			    
+			}
+
+			private void add(JButton restartButton2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+			private class ButtonListener implements ActionListener {
+				public void actionPerformed(ActionEvent event) {
+					new SpaceInvaders();
+
+				}
+	}
+
+	/*
+	 * Getters & Setters
+	 */
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+}
---
+++ PushPanel.java
@@ -0,0 +1,34 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * 
+ * @author
+ */
+public class PushPanel extends JPanel {
+	
+	/**
+	 * 
+	 */
+
+	private JButton push;
+
+	/*
+	 * Constructor
+	 */
+	public PushPanel() {
+		push = new JButton("start");
+		push.addActionListener(new ButtonListener());
+		push.setBounds(800, 800, 200, 100);
+
+		add(push);
+
+	}
+
+	private class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			new SpaceInvaders();
+
+		}
+	}
+}
